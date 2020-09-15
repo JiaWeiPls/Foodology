ac656a6f7923aa3adb4900a0c8928615826216f8
wlim087 <65529631+wlim087@users.noreply.github.com>
2020-09-15 20:57:11 +0800

    Delete hello.java

--- hello.java
+++
@@ -1,9 +0,0 @@
-/*
- * First Java program, which says hello.
- */
-public class Hello {   // Save as "Hello.java"
-   public static void main(String[] args) {  // Program entry point
-      System.out.println("Hello! I love potatoes");   // Print text message
-      // Hello
-   }
-}
